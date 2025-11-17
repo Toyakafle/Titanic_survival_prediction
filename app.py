7e92ffd491cab8d21e67e8d51f3be5f4fe7374cd
Toyakafle <kafle.bisu30@gmail.com>
2025-11-17 16:08:08 +0545

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Load model
-with open("models/model.pkl", "rb") as f:
+with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
 st.title("Titanic Survival Prediction")
